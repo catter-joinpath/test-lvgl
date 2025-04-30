fbe452a19940667da12bb914657d664e1965d3f4
Catter Wang <catter.wang@joinpathm.com>
2025-04-30 14:44:01 +0800

    add gesture callback, and switch between screens

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <lvgl/lv_conf.h>
 #include <lvgl/lvgl.h>
 #include "ui/ui.h"
+#include "ui/screens.h"
+
+#define FIRST_SCREEN SCREEN_ID_HEATING_SCREEN
+#define LAST_SCREEN SCREEN_ID_LIGHTING_SCREEN
+
+static void gesture_cb(lv_event_t * e)
+{
+    lv_dir_t dir = lv_indev_get_gesture_dir(lv_indev_active());
+    int screen = eez_flow_get_current_screen();
+    switch (dir) {
+    case LV_DIR_TOP:
+        printf("LV_DIR_TOP\n");
+        screen = (screen - 1) >= FIRST_SCREEN ? (screen - 1) : LAST_SCREEN;
+        eez_flow_set_screen(screen, LV_SCR_LOAD_ANIM_MOVE_TOP, 200, 0);
+        break;
+    case LV_DIR_BOTTOM:
+        printf("LV_DIR_BOTTOM\n");
+        screen = (screen + 1) <= LAST_SCREEN ? (screen + 1) : FIRST_SCREEN;
+        eez_flow_set_screen(screen, LV_SCR_LOAD_ANIM_MOVE_BOTTOM, 200, 0);
+        break;
+    default:
+        printf("unsupported %d\n", dir);
+        break;
+    }
+}
 
 int main(int args, char **argv)
 {
@@ -12,6 +38,8 @@ int main(int args, char **argv)
     char *path = lv_libinput_find_dev(LV_LIBINPUT_CAPABILITY_TOUCH, true);
     lv_indev_t *indev = lv_libinput_create(LV_INDEV_TYPE_POINTER, path);
     lv_indev_set_display(indev, display);
+
+    lv_indev_add_event_cb(indev, gesture_cb, LV_EVENT_GESTURE, NULL);
     
     ui_init();
     while (1)
